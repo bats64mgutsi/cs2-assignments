de771ae0d84fd1b054417c8fa69eb158a7af3889
Batandwa Mgutsi <B.mgutsi@gmail.com>
2021-04-05 11:09:58 +0200

    Adds locator

---
+++ bst-vs-simple-arrays/src/Locator.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class for managing application dependencies.
+ * 
+ * The idea is to separate dependency creation from usage.
+ */
+public abstract class Locator {
+  /**
+   * Returns the first instance of T it finds in the registered dependencies.n
+   */
+  public abstract Object get(Class<?> of);
+
+  public abstract void registerSingleton(Object instance);
+
+  public static final Locator instance = new LocatorImpl();
+
+  private static class LocatorImpl extends Locator {
+    final List<Object> dependencies = new ArrayList<>();
+
+    @Override
+    public void registerSingleton(Object instance) {
+      dependencies.add(instance);
+    }
+
+    @Override
+    public Object get(Class<?> of) {
+      for (Object e : dependencies) {
+        if (of.isInstance(e))
+          return e;
+      }
+
+      return null;
+    }
+
+  }
+}
