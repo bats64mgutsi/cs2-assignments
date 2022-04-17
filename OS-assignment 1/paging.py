6349ab25e38b017c1f0b3d42ff6d50cf9a698b88
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-04-18 01:16:53 +0200

    Adds all algorithms

---
+++ OS-assignment 1/paging.py
@@ -0,0 +1,110 @@
+from collections import deque
+from heapq import heapify, heappop, heappush
+from typing import Tuple
+
+# Utils ---------
+
+currentTimeStamp = 0
+
+
+def getCurrentTimestamp() -> int:
+    global currentTimeStamp
+    currentTimeStamp += 1
+    return currentTimeStamp
+
+
+def getPageLRUPageTableEntryIndex(pageNumber: int, pageTable: list[Tuple[float, int]]) -> int:
+    for index, (_, entryPageNumber) in enumerate(pageTable):
+        if entryPageNumber == pageNumber:
+            return index
+
+    return -1
+
+
+def lruPageTableEntryExist(pageNumber: int, pageTable: list[Tuple[float, int]]) -> bool:
+    return getPageLRUPageTableEntryIndex(pageNumber, pageTable) != -1
+
+
+def countStepsToNextTouchForPage(pageNumber: int, inReferenceString: list[int], cursor: int) -> int:
+    if cursor >= len(inReferenceString):
+        return 0
+
+    forwardCollection = inReferenceString[cursor:]
+    for subIndex, nextPageNumber in enumerate(forwardCollection):
+        if nextPageNumber == pageNumber:
+            return subIndex
+
+    return len(forwardCollection)
+
+
+def getCandidateIndexForPageWithHighestStepsToNextCount(candidatePageNumbers: list[int], inReferenceString: list[int], cursor: int) -> int:
+    selectedPageIndex = 0
+    selectedPageStepsToNextTouchCount = countStepsToNextTouchForPage(
+        candidatePageNumbers[0], inReferenceString, cursor)
+
+    for iii, candidatePageNumber in enumerate(candidatePageNumbers):
+        stepsCount = countStepsToNextTouchForPage(
+            candidatePageNumber, inReferenceString, cursor)
+        if stepsCount > selectedPageStepsToNextTouchCount:
+            selectedPageIndex = iii
+            selectedPageStepsToNextTouchCount = stepsCount
+
+    return selectedPageIndex
+
+# Actual algorithms
+
+
+def fifo(frameCount: int, pageReferences: list[int]) -> int:
+    """Runs the FIFO algorithm on the given test string and returns the number of page faults"""
+    pageTable = deque([], maxlen=frameCount)
+    pageFaults = 0
+    for pageNumber in pageReferences:
+        if pageNumber in pageTable:
+            continue
+        else:
+            pageFaults += 1
+            pageTable.append(pageNumber)
+
+    return pageFaults
+
+
+def lru(frameCount: int, pageReferences: list[int]) -> int:
+    """Runs the LRU algorithm on the given test string and returns the number of page faults"""
+    pageTable = list[Tuple[float, int]]()
+    pageFaults = 0
+    for pageNumber in pageReferences:
+        if lruPageTableEntryExist(pageNumber, pageTable):
+            # Touch the entry
+            entryIndex = getPageLRUPageTableEntryIndex(pageNumber, pageTable)
+            pageTable[entryIndex] = (getCurrentTimestamp(), pageNumber)
+            heapify(pageTable)
+        else:
+            pageFaults += 1
+
+            if len(pageTable) == frameCount:
+                heappop(pageTable)
+
+            heappush(pageTable, (getCurrentTimestamp(), pageNumber))
+
+    return pageFaults
+
+
+def opt(frameCount: int, pageReferences: list[int]) -> int:
+    """Runs the OPT algorithm on the given test string and returns the number of page faults"""
+    pageTable = list[int]()
+    pageFaults = 0
+    for cursor, pageNumber in enumerate(pageReferences):
+        if pageNumber in pageTable:
+            continue
+        else:
+            pageFaults += 1
+
+            if len(pageTable) == frameCount:
+                indexToReplace = getCandidateIndexForPageWithHighestStepsToNextCount(
+                    pageTable, pageReferences, cursor)
+                pageTable[indexToReplace] = pageNumber
+                pass
+            else:
+                pageTable.append(pageNumber)
+
+    return pageFaults
---
+++ OS-assignment 1/paging_test.py
@@ -0,0 +1,18 @@
+
+from paging import countStepsToNextTouchForPage, fifo, getCandidateIndexForPageWithHighestStepsToNextCount, lru, opt
+
+
+TEST_STRING = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
+FRAME_COUNT = 3
+
+assert countStepsToNextTouchForPage(2, [2, 3, 2, 4, 5], 1) == 1, "test countStepsToNextTouchForPage with cursor"
+assert countStepsToNextTouchForPage(7, [2, 3, 0, 7, 5], 1) == 2, "test countStepsToNextTouchForPage after cursor but before end"
+assert countStepsToNextTouchForPage(5, [2, 3, 0, 7, 5], 1) == 3, "test countStepsToNextTouchForPage end"
+
+assert getCandidateIndexForPageWithHighestStepsToNextCount([7, 0, 1], TEST_STRING, 3) == 0, "test getCandidateIndexForPageWithLowestStepsToNextCount"
+
+assert fifo(FRAME_COUNT, TEST_STRING) == 15, "test FIFO"
+assert lru(FRAME_COUNT, TEST_STRING) == 12, "test LRU"
+assert opt(FRAME_COUNT, TEST_STRING) == 9, "test OPT"
+
+print("ALL PASSED")
